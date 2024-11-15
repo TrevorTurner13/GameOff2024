27bc68cc393a0ed3d836b85056b64938b467d7d0
quinkennett <quinton.kennett@easternstudent.ca>
2024-11-15 12:02:31 -0400

    Added player respawn on death. Currently set to players starting position

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,6 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int playerHealth;
     [SerializeField] private AudioClip[] hurtSFX;
+    [SerializeField] private Vector3 respawnPosition;
 
     public int currentHealth;
 
@@ -14,9 +15,12 @@ public class PlayerHealth : MonoBehaviour
     public Healthbar healthBar;
 
     public bool isDead;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        respawnPosition = transform.position;
         currentHealth = playerHealth;
         healthBar.SetMaxHealth(playerHealth);
     }
@@ -24,18 +28,18 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerHealth <= 0)
+        if (currentHealth <= 0)
         {
             Die();
         }
+
     }
 
     public void TakeDamage()
     {
         if (playerHealth > 0)
         {
-            playerHealth -= 10;
-            currentHealth = playerHealth;
+            currentHealth -= 10;
             animator.SetTrigger("isHurt");
             SFXManager.instance.PlayRandomSFXClip(hurtSFX, transform, 0.1f);
             healthBar.SetHealth(currentHealth);
@@ -49,5 +53,31 @@ public class PlayerHealth : MonoBehaviour
         animator.SetBool("isDead", true);
         animator.SetBool("isRunning", false);
         animator.SetBool("isFalling", false);
+
+        if (isDead)
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+
+        StartCoroutine(WaitForDeath());
+          
+    }
+
+
+    IEnumerator WaitForDeath()
+    {
+        currentHealth = playerHealth;
+
+        yield return new WaitForSeconds(3);
+
+        animator.SetBool("isDead", false);
+        isDead = false;
+        transform.position = respawnPosition;
+        healthBar.SetMaxHealth(playerHealth);
+
     }
 }
