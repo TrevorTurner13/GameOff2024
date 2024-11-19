bffd60e50a97bc4c64c470300174a45a9dd0a625
quinkennett <quinton.kennett@easternstudent.ca>
2024-11-19 14:48:36 -0400

    Reworked inventory system, player now can pick up item and its added to inventory slot, just need to add use item functionality

---
+++ Assets/Scripts/Inventory/ItemUISlot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemUISlot : MonoBehaviour
+{
+    public string itemName;
+    public int itemQuantity;
+    public Sprite itemSprite;
+    public bool isFull;
+
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text quantityText;
+
+    public void AddItem(string itemName, int itemQuantity, Sprite itemSprite)
+    {
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+        this.itemSprite = itemSprite;
+        isFull = true;
+
+        quantityText.text = itemQuantity.ToString();
+        quantityText.enabled = true;
+        itemImage.sprite = itemSprite;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
